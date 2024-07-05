f3016dc7b33b8100e9b697dbdd19353180dcec46
Riyadus Salehin <166088594+riyad-x@users.noreply.github.com>
2024-07-05 12:51:28 +0600

    Add files via upload

---
+++ backtrack.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            } else if(s[i] == ')' || s[i] == '}' || s[i] == ']'){
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if((s[i] == ')' && top!= '(') || (s[i] == '}' && top!= '{') || (s[i] == ']' && top!= '[')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main(){
+    system("cls");
+    Solution s;
+    string trck;
+    cin>>trck;
+    cout<<(s.isValid(trck) ? "true" : "false");
+    return 0;
+}
