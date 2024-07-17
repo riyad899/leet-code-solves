c5335c9f14d7620dd3a34dff8f2604cac95e0432
Riyadus Salehin <166088594+riyad-x@users.noreply.github.com>
2024-07-17 23:16:55 +0600

    Add files via upload

---
+++ sqart.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mysqrt(int x){
+    int s=sqrt(x);
+    return s;
+}
+
+int main(){
+    int x;
+    cin>>x;
+    cout<<mysqrt(x)<<endl; // Call the mysqrt function
+    return 0; // Add return 0 at the end of main function
+}
